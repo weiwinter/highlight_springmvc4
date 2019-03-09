1801c052ab1d91b40e2b3a7376ce87d47a37ea84
weiwinter <837144154@qq.com>
2019-03-09 17:57:04 +0800

    SpringMVC项目demo之highlight_springmvc4

---
+++ src/main/java/com/wisely/highlight_springmvc4/MyMvcConfig.java
@@ -0,0 +1,31 @@
+package com.wisely.highlight_springmvc4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.wisely.highlight_springmvc4")
+public class MyMvcConfig extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = 
+				new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/classes/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		return viewResolver;
+	}
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+	}
+}
