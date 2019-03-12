5675232456ceae9757095fb1e1aa8e41a3b8e02d
weiwinter <837144154@qq.com>
2019-03-12 21:17:28 +0800

    20190312的开发-4.4.3-@ControllerAdvice

---
+++ src/main/java/com/wisely/highlight_springmvc4/advice/ExceptionHandlerAdvice.java
@@ -0,0 +1,31 @@
+package com.wisely.highlight_springmvc4.advice;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+	@ExceptionHandler(value = Exception.class)
+	public ModelAndView exception(Exception exception, WebRequest request){
+		ModelAndView model = new ModelAndView("error");
+		model.addObject("errorMessage", exception.getMessage());
+		return model;
+	}
+	
+	@ModelAttribute
+	public void addAttributes(Model model){
+		model.addAttribute("msg", "额外信息");
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder){
+		webDataBinder.setDisallowedFields("id");
+	}
+}
